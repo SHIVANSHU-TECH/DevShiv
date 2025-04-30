0a4dcc4bb772dd16b196c4eebcdc10a831cd0cc6
SHIVANSHU-TECH <Shivanshushuklacse@delhitechnicalcampus.ac.in>
2025-04-30 18:39:13 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { Club, ChevronDown, Code, Monitor, Users, Award, ArrowRight, Github, Lin
 import * as THREE from 'three';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { motion } from 'framer-motion'
-import { db } from '../firebase';
+import { db } from '../lib/firebase';
 
 export default function Portfolio() {
   const [activeSection, setActiveSection] = useState('hero');
